fd9e67101d2fc64def5c4ba14a79697967019a2b
fullmoonhalf <fullmoonhalf@users.noreply.github.com>
2021-05-11 17:05:53 +0900

    #2 Singleton/BootSequence まわり、 #4 デバッグログ出力

---
+++ Assets/Until/Scripts/Develop/Log.cs
@@ -0,0 +1,78 @@
+﻿using System.Diagnostics;
+
+
+namespace until.develop
+{
+    public static class Log
+    {
+        [Conditional("TEST")]
+        public static void fatal(object Source, string Text, params object[] Args)
+        {
+            var Message = $"[{Source.GetType().Name}] " + string.Format(Text, Args);
+            UnityEngine.Debug.LogAssertionFormat(Message);
+        }
+
+        [Conditional("TEST")]
+        public static void fatal(string Source, string Text, params object[] Args)
+        {
+            var Message = $"[{Source}] " + string.Format(Text, Args);
+            UnityEngine.Debug.LogAssertionFormat(Message);
+        }
+
+        [Conditional("TEST")]
+        public static void error(object Source, string Text, params object[] Args)
+        {
+            var Message = $"[{Source.GetType().Name}] " + string.Format(Text, Args);
+            UnityEngine.Debug.LogErrorFormat(Message);
+        }
+
+        [Conditional("TEST")]
+        public static void error(string Source, string Text, params object[] Args)
+        {
+            var Message = $"[{Source}] " + string.Format(Text, Args);
+            UnityEngine.Debug.LogErrorFormat(Message);
+        }
+
+        [Conditional("TEST")]
+        public static void warning(object Source, string Text, params object[] Args)
+        {
+            var Message = $"[{Source.GetType().Name}] " + string.Format(Text, Args);
+            UnityEngine.Debug.LogWarningFormat(Message);
+        }
+
+        [Conditional("TEST")]
+        public static void warning(string Source, string Text, params object[] Args)
+        {
+            var Message = $"[{Source}] " + string.Format(Text, Args);
+            UnityEngine.Debug.LogWarningFormat(Message);
+        }
+
+        [Conditional("TEST")]
+        public static void info(object Source, string Text, params object[] Args)
+        {
+            var Message = $"[{Source.GetType().Name}] " + string.Format(Text, Args);
+            UnityEngine.Debug.LogFormat(Message);
+        }
+
+        [Conditional("TEST")]
+        public static void info(string Source, string Text, params object[] Args)
+        {
+            var Message = $"[{Source}] " + string.Format(Text, Args);
+            UnityEngine.Debug.LogFormat(Message);
+        }
+
+        [Conditional("VERBOSE")]
+        public static void verbose(object Source, string Text, params object[] Args)
+        {
+            var Message = $"[{Source.GetType().Name}] " + string.Format(Text, Args);
+            UnityEngine.Debug.LogFormat(Message);
+        }
+
+        [Conditional("VERBOSE")]
+        public static void verbose(string Source, string Text, params object[] Args)
+        {
+            var Message = $"[{Source}] " + string.Format(Text, Args);
+            UnityEngine.Debug.LogFormat(Message);
+        }
+    }
+}
---
+++ Assets/Until/Scripts/Systems/BootSystem.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using until.develop;
+
+
+namespace until.system.singleton
+{
+    public class BootSystem : Singleton<BootSystem>
+    {
+        #region Definitions.
+        public enum Phase
+        {
+            None,
+            SystemOnWork,
+        }
+        #endregion
+
+        #region Properties.
+        public Phase CurrentPhase { get; private set; } = Phase.None;
+        public uint FrameCount = 0;
+        #endregion
+
+        #region Singleton
+        public override void onSingletonAwake()
+        {
+        }
+
+        public override void onSingletonStart()
+        {
+        }
+
+        public override void onSingletonDestroy()
+        {
+        }
+        #endregion
+
+        #region Process Control
+        public void onUpdate()
+        {
+            ++FrameCount;
+
+            switch (CurrentPhase)
+            {
+                case Phase.None:
+                    transit(Phase.SystemOnWork);
+                    break;
+                case Phase.SystemOnWork:
+                    break;
+            }
+        }
+
+        private void transit(Phase NextPhase)
+        {
+            Log.info(this, $"transit {CurrentPhase} -> {NextPhase}");
+            CurrentPhase = NextPhase;
+        }
+        #endregion
+    }
+}
+
