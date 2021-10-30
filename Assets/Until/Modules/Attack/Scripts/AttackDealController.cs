53f0b970ed178d6e031c391aa94263c4d5a19550
fullmoonhalf <fullmoonhalf@users.noreply.github.com>
2021-10-30 19:27:24 +0900

    Attack Dealing のフレームワーク

---
+++ Assets/Until/Modules/Attack/Scripts/AttackDealController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using until.system;
+
+
+namespace until.modules.attack
+{
+    [DefaultExecutionOrder(system.defines.ExecutionOrder.Develop_Tail_10)]
+    public class AttackDealController : Behavior
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Singleton.AttackDealManager.resolve();
+        }
+    }
+}
+
---
+++ Assets/Until/Modules/Attack/Scripts/AttackDealTarget.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace until.modules.attack
+{
+    public interface AttackDealTarget : AttackDealEntity
+    {
+    }
+}
+
