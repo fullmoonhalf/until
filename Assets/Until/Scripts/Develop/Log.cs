82905b5148e114ac17a594b51cdc143a8381f24c
fullmoonhalf <fullmoonhalf@users.noreply.github.com>
2021-12-31 01:09:58 +0900

    [BugFix] 本体テキストのログ出力がされない問題修正。

--- Assets/Until/Scripts/Develop/Log.cs
+++ Assets/Until/Scripts/Develop/Log.cs
@@ -8,70 +8,70 @@ namespace until.develop
         [Conditional("TEST")]
         public static void fatal(object Source, string Text, params object[] Args)
         {
-            var Message = $"[{Source.GetType().FullName}] " + string.Join(" ", Args);
+            var Message = $"[{Source.GetType().FullName}] {Text}" + string.Join(" ", Args);
             UnityEngine.Debug.LogAssertionFormat(Message);
         }
 
         [Conditional("TEST")]
         public static void fatal(string Source, string Text, params object[] Args)
         {
-            var Message = $"[{Source}] " + string.Join(" ", Args);
+            var Message = $"[{Source}] {Text}" + string.Join(" ", Args);
             UnityEngine.Debug.LogAssertionFormat(Message);
         }
 
         [Conditional("TEST")]
         public static void error(object Source, string Text, params object[] Args)
         {
-            var Message = $"[{Source.GetType().FullName}] " + string.Join(" ", Args);
+            var Message = $"[{Source.GetType().FullName}] {Text}" + string.Join(" ", Args);
             UnityEngine.Debug.LogErrorFormat(Message);
         }
 
         [Conditional("TEST")]
         public static void error(string Source, string Text, params object[] Args)
         {
-            var Message = $"[{Source}] " + string.Join(" ", Args);
+            var Message = $"[{Source}] {Text}" + string.Join(" ", Args);
             UnityEngine.Debug.LogErrorFormat(Message);
         }
 
         [Conditional("TEST")]
         public static void warning(object Source, string Text, params object[] Args)
         {
-            var Message = $"[{Source.GetType().FullName}] " + string.Join(" ", Args);
+            var Message = $"[{Source.GetType().FullName}] {Text}" + string.Join(" ", Args);
             UnityEngine.Debug.LogWarningFormat(Message);
         }
 
         [Conditional("TEST")]
         public static void warning(string Source, string Text, params object[] Args)
         {
-            var Message = $"[{Source}] " + string.Join(" ", Args);
+            var Message = $"[{Source}] {Text}" + string.Join(" ", Args);
             UnityEngine.Debug.LogWarningFormat(Message);
         }
 
         [Conditional("TEST")]
         public static void info(object Source, string Text, params object[] Args)
         {
-            var Message = $"[{Source.GetType().FullName}] " + string.Join(" ", Args);
+            var Message = $"[{Source.GetType().FullName}] {Text}" + string.Join(" ", Args);
             UnityEngine.Debug.LogFormat(Message);
         }
 
         [Conditional("TEST")]
         public static void info(string Source, string Text, params object[] Args)
         {
-            var Message = $"[{Source}] " + string.Join(" ", Args);
+            var Message = $"[{Source}] {Text}" + string.Join(" ", Args);
             UnityEngine.Debug.LogFormat(Message);
         }
 
         [Conditional("VERBOSE")]
         public static void verbose(object Source, string Text, params object[] Args)
         {
-            var Message = $"[{Source.GetType().FullName}] " + string.Join(" ", Args);
+            var Message = $"[{Source.GetType().FullName}] {Text}" + string.Join(" ", Args);
             UnityEngine.Debug.LogFormat(Message);
         }
 
         [Conditional("VERBOSE")]
         public static void verbose(string Source, string Text, params object[] Args)
         {
-            var Message = $"[{Source}] " + string.Join(" ", Args);
+            var Message = $"[{Source}] {Text}" + string.Join(" ", Args);
             UnityEngine.Debug.LogFormat(Message);
         }
     }
