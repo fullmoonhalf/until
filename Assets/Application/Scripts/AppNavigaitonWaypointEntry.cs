de343bf83e2bc8ef2eaa1a4fd94f378b4e2d15b3
fullmoonhalf <fullmoonhalf@users.noreply.github.com>
2022-01-02 01:24:36 +0900

    waypoint の可視化

--- Assets/Application/Scripts/AppNavigaitonWaypointEntry.cs
+++ Assets/Application/Scripts/AppNavigaitonWaypointEntry.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEditor;
 using until.system;
 using until.develop;
 
@@ -14,11 +15,46 @@ namespace until.test
     [DefaultExecutionOrder(until.system.settings.UntilBehaviorOrder.System_SettingBehavior)]
     public class AppNavigaitonWaypointEntry : SettingBehavior
     {
-        public Vector3 Position { get; private set; }
+        #region inspector
+        public AppNavigaitonWaypointEntry[] NextPoint = null;
+        #endregion
 
+        #region properties
+        public Vector3 Position { get; private set; }
+        #endregion
+
+        #region methods
         void Awake()
         {
             Position = gameObject.transform.position;
         }
+
+        #region Develop
+#if TEST
+        void OnDrawGizmos()
+        {
+            var source = gameObject.transform.position;
+            Gizmos.DrawWireCube(source, Vector3.one);
+            if (NextPoint != null)
+            {
+                foreach (var next in NextPoint)
+                {
+                    if (next != null)
+                    {
+                        var target = next.gameObject.transform.position;
+                        var half = (source + target) * 0.5f;
+                        Gizmos.DrawLine(source, half);
+                    }
+                }
+            }
+        }
+
+        void OnDrawGizmosSelected()
+        {
+            Gizmos.DrawWireSphere(gameObject.transform.position, 2.0f);
+        }
+#endif
+        #endregion
+        #endregion
     }
 }
--- Assets/Application/Scripts/AppNavigationWaypointsCollection.cs
+++ Assets/Application/Scripts/AppNavigationWaypointsCollection.cs
@@ -44,5 +44,54 @@ namespace until.test
             _RefLevelDatabase.Waypoints = null;
         }
         #endregion
+
+        #region Develop
+#if TEST
+        [ContextMenu(nameof(fixupLink))]
+        private void fixupLink()
+        {
+            // インデックスを決める
+            var waypoints = gameObject.GetComponentsInChildren<AppNavigaitonWaypointEntry>();
+            var wayindecies = new Dictionary<AppNavigaitonWaypointEntry, int>();
+            foreach (var waypoint in waypoints)
+            {
+                var index = wayindecies.Count;
+                wayindecies.Add(waypoint, index);
+            }
+
+            // リンク構造を決める
+            var linkdb = new bool[waypoints.Length, waypoints.Length];
+            for (int index = 0; index < waypoints.Length; ++index)
+            {
+                var myself_waypoint = waypoints[index];
+                var myself_index = wayindecies[myself_waypoint];
+                for (int subidx = 0; subidx < myself_waypoint.NextPoint.Length; ++subidx)
+                {
+                    var next_waypoint = myself_waypoint.NextPoint[subidx];
+                    var next_index = wayindecies[next_waypoint];
+                    linkdb[myself_index, next_index] = true;
+                    linkdb[next_index, myself_index] = true;
+                }
+            }
+
+            // 足りないのを追加する。
+            for (int myself = 0; myself < waypoints.Length; ++myself)
+            {
+                var neighbours = new List<AppNavigaitonWaypointEntry>();
+                for (int target = 0; target < waypoints.Length; ++target)
+                {
+                    if (linkdb[myself, target])
+                    {
+                        var target_waypoint = waypoints[target];
+                        neighbours.Add(target_waypoint);
+                    }
+                }
+
+                var myself_waypoint = waypoints[myself];
+                myself_waypoint.NextPoint = neighbours.ToArray();
+            }
+        }
+#endif
+        #endregion
     }
 }
